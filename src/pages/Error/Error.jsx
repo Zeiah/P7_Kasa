31f00ee2683e53a4e51592a9a204f85cef99c90c
Margot Salsmann <margot.salsmann@gmail.com>
2023-03-22 13:07:10 +0100

    page erreur (responsive ok)

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -5,9 +5,7 @@ export default function Error() {
     return (
         <div className="error">
             <h1 className="error__404"> 404 </h1>
-            <p className="error__infos">
-                Oups! La page que vous demandez n'existe pas.
-            </p>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
             <Link to="/" className="error__link">
                 Retourner sur la page d'accueil
             </Link>
